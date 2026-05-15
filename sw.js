86e9cf06973196f1fa137b416a63b52f9a9b953f
QRally <sales@rallye-historique.co.uk>
2026-05-15 13:52:31 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,17 @@
+const cacheName = 'rally-rm-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/gpx.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
